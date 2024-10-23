09f6b3e88faab151274b7adc589d8eec9dda7303
ferha <ferha@FERHAT>
2024-10-23 16:28:50 +0300

    Add project files.

---
+++ SayiTahmin/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace SayiTahmin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.txtGuess = new System.Windows.Forms.TextBox();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.Location = new System.Drawing.Point(212, 207);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(84, 52);
+            this.btnGuess.TabIndex = 0;
+            this.btnGuess.Text = "Tahmin Et";
+            this.btnGuess.UseVisualStyleBackColor = true;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // txtGuess
+            // 
+            this.txtGuess.Location = new System.Drawing.Point(372, 71);
+            this.txtGuess.Multiline = true;
+            this.txtGuess.Name = "txtGuess";
+            this.txtGuess.Size = new System.Drawing.Size(100, 20);
+            this.txtGuess.TabIndex = 2;
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(50, 71);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(35, 13);
+            this.lblMessage.TabIndex = 3;
+            this.lblMessage.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(532, 390);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.txtGuess);
+            this.Controls.Add(this.btnGuess);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.TextBox txtGuess;
+        private System.Windows.Forms.Label lblMessage;
+    }
+}
+
---
+++ SayiTahmin/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SayiTahmin
+{
+    public partial class Form1 : Form
+    {
+        private int randomSayi;  
+        private int deneme = 5;
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeGame();
+        }
+        private void InitializeGame()
+        {
+            Random random = new Random();
+            randomSayi = random.Next(1, 101);  
+            deneme = 5;  
+            lblMessage.Text = "1 ile 100 arasında bir sayı tahmin edin. 5 hakkınız var.";  
+            txtGuess.Clear();  
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtGuess.Text, out int userGuess))
+            {
+                deneme--;  
+
+                if (userGuess == randomSayi)
+                {
+                    MessageBox.Show("Tebrikler! Sayıyı doğru tahmin ettiniz!", "Başarı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    InitializeGame();  // Oyun baştan başlasın
+                }
+                else if (userGuess > randomSayi)
+                {
+                    lblMessage.Text = $"Tahmininiz büyük! {deneme} hakkınız kaldı.";
+                }
+                else
+                {
+                    lblMessage.Text = $"Tahmininiz küçük! {deneme} hakkınız kaldı.";
+                }
+
+                if (deneme == 0)
+                {
+                    MessageBox.Show($"Üzgünüm, hakkınız doldu. Doğru sayı {randomSayi} idi.", "Başarısızlık", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    InitializeGame();  // Oyun baştan başlasın
+                }
+            }
+            else
+            {
+                MessageBox.Show("Geçerli bir sayı giriniz!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
